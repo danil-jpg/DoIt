bb16c22f49992e981e6602ef08856d1ae5609468
Danil <danil.polovinkin.1994@gmail.com>
2023-06-22 11:39:44 +0300

    test

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,19 +1,17 @@
-import Container from "../../containers/Container/Container";
-import React from "react";
-import s from "./Home.module.scss"
-import HeroBlock from "./HeroBlock/HeroBlock";
+import Container from '../../containers/Container/Container';
+import React from 'react';
+import s from './Home.module.scss';
+import HeroBlock from './HeroBlock/HeroBlock';
 const Home = (props) => {
-    return ( 
-        <div className={s.main}>
-            <div className={s.main__wrapper}>
-                <div className={s.main__hero}>
-                   <Container>
-                  {/* <HeroBlock/> */}
-                   </Container>
-                </div>
-            </div>
+  return (
+    <div className={s.main}>
+      <div className={s.main__wrapper}>
+        <div className={s.main__hero}>
+          <Container>{/* <HeroBlock/> */}</Container>
         </div>
-     );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default Home;
