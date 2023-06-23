05291a0ba7ac1acda899bc6157ad439a445dfdf5
Danil <danil.polovinkin.1994@gmail.com>
2023-06-23 10:09:17 +0300

    some profile fixes

---
+++ src/components/pages/Profile/PlayerAwards/PlayerAwards.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import s from './playerAwards.module.scss';
+
+const PlayerAwards = () => {
+  return <div className={s.award}></div>;
+};
+
+export default PlayerAwards;
--- src/components/pages/Profile/Profile.jsx
+++ src/components/pages/Profile/Profile.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import s from './profile.module.scss';
 import Container from '../../containers/Container/Container';
 import PlayerStat from './PlayerStat/PlayerStat';
+import PlayerData from './PlayerData/PlayerData';
+import PlayerAwards from './PlayerAwards/PlayerAwards';
 
 const Profile = () => {
   return (
     <div className={s.profile}>
-      <Container>
+      <Container className={s.cont}>
         <div className={s.hero}>
           <PlayerStat />
+          <PlayerData />
+          <PlayerAwards />
         </div>
       </Container>
     </div>
