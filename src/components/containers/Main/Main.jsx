7ae35c14030538e46f05d6622af795a8b8774637
Danil <danil.polovinkin.1994@gmail.com>
2023-06-26 13:20:38 +0300

    added base template of popup

--- src/components/containers/Main/Main.jsx
+++ src/components/containers/Main/Main.jsx
@@ -1,23 +1,25 @@
 import React, { Suspense } from 'react';
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 import s from './Main.module.scss';
-import Header from "../../common/Header/Header";
+import Header from '../../common/Header/Header';
 
 const Home = React.lazy(() => import('../../pages/Home/Home.jsx'));
 const News = React.lazy(() => import('../../pages/News/News.jsx'));
-const Premium = React.lazy(() => import("../../pages/Premium/Premium.jsx"));
+const Premium = React.lazy(() => import('../../pages/Premium/Premium.jsx'));
 const Profile = React.lazy(() => import('../../pages/Profile/Profile.jsx'));
 const Dashboard = React.lazy(() => import('../../pages/Dashboard/Dashboard.jsx'));
+import PopUpTemp from '../../common/PopUp/PopUpTemp';
 
 const Main = (props) => {
   return (
     <main className={s.main}>
       <Header />
+      <PopUpTemp />
       <Suspense fallback={<>Loading</>}>
         <Routes>
           <Route index element={<Home />} />
-          <Route path="/news" element={<News />} />
-          <Route path="/premium" element={<Premium />} />
+          <Route path='/news' element={<News />} />
+          <Route path='/premium' element={<Premium />} />
           <Route path='/profile' element={<Profile />} />
           <Route path='/admin/*' element={<Dashboard />} />
         </Routes>
