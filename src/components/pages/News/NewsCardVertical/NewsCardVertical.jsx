79e6aa9a5ea482d406aac60238b089bc88cf07f9
Maksym Puzanov <puzanov.maksym@gmail.com>
2023-07-13 08:59:27 +0300

    update news page

--- src/components/pages/News/NewsCardVertical/NewsCardVertical.jsx
+++ src/components/pages/News/NewsCardVertical/NewsCardVertical.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import s from "./NewsCardVertical.module.scss"
 import Picture from "../../../UI/Picture/Picture";
 const NewsCardVertical = (props) => {
-    return ( 
+    return (
         <div className={s.card}>
             <div className={s.card__wrapper}>
-            <div className={s.card__image}>
-            <Picture 
-                 img={props.img}
-                 webp={props.img__webp}
-                 alt={props.alt} 
-                 imgMob={props.imgMob}
-                 webpMob={props.webpMob} 
-                 widthMob='490'
-                 />
+                <div className={s.card__imageWrap}>
+                    <Picture
+                        img={props.img}
+                        webp={props.img__webp}
+                        alt={props.alt}
+                        imgMob={props.imgMob}
+                        webpMob={props.webpMob}
+                        widthMob='490'
+                        className={s.card__image}
+                    />
                 </div>
                 <div className={s.card__info}>
                     <div className={s.card__top}>
@@ -26,7 +27,7 @@ const NewsCardVertical = (props) => {
                 </div>
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default NewsCardVertical;
